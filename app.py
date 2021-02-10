34127ccf7fa7a190bbdf9060a788ae5b40d5d269
surya-bug <surya.rubyist@gmail.com>
2021-02-10 22:37:39 +0530

    Added store list and implemented get and post apis

--- app.py
+++ app.py
@@ -2,11 +2,60 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+
+    {
+    
+    'name' : 'My Wonderful Store',
+    'items' : [
+    
+            {
+        
+                'name' : 'My Item',
+                'price' : 15.99
+        
+        
+            }
+        
+        ]
+    
+
+    
+    
+    
+    }
+
+
+]
+
 @app.route("/") # 'https://www.yoursite.com/"
 
-def home():
-    return "Hello World !"
+# POST - used to receive data
+# GET - send data back only to the browser
+
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
     
+    
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
+    
+
+@app.route("/store")
+def get_store():
+    pass
+
+ 
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    pass
+ 
+    
+@app.route("/store/<string:name>/item")
+def get_item_in_store(name):
+    pass
     
     
 app.run(port=8080)
